6e01f567e2e96a75c07516f898c6d01b44c516db
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2023-10-11 23:35:02 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 536 ms, Memory: 294208 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,47 +1,43 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
- 
-        Queue<Integer> q = new LinkedList<>();
-        
-        // Queue에 1~N까지 값을 offer한다.
-        for (int i = 1; i <= N; i++) {
-            q.offer(i);
-        }
- 
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        
-        // Queue의 사이즈가 1일 때까지 반복한다.
-        while(q.size() != 1) {
-            // K - 1번째까지는 처음에 있던 값을 맨 뒤로 보낸다.
-            for (int i = 0; i < K - 1; i++) {
-                q.offer(q.poll());
-            }
-            // K번째 값은 poll한 후 출력한다.
-            sb.append(q.poll() + ", ");
-        }
-        
-        // Queue의 사이즈가 1일 때는 단순히 poll하면 된다.
-        sb.append(q.poll() + ">");
- 
-        bw.write(sb.toString() + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		Queue<Integer> qu = new LinkedList<>();
+		for (int i = 1; i <= n; i++) {
+			qu.offer(i);
+		}
+		sb.append("<");
+		while (qu.size() != 1) {
+			for (int i = 0; i < k - 1; i++) {
+				qu.offer(qu.poll());
+			}
+			sb.append(qu.poll() + ", ");
+		}
+//		int count = 0;
+//		while (qu.size() != 1) {
+//			qu.offer(qu.poll());
+//			count++;
+//			if (count == k - 1) {
+//				count = 0;
+//				sb.append(qu.poll() + ", ");
+//			}
+//		}
+		sb.append(qu.poll());
+		sb.append(">");
+		System.out.println(sb);
+	}
 }
