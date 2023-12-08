4cd3e30848794f6bf946a5acb0f7464675da401f
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2023-12-08 19:01:03 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11288 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(
+                "         ,r'\"7\n" +
+                        "r`-_   ,'  ,/\n" +
+                        " \\. \". L_r'\n" +
+                        "   `~\\/\n" +
+                        "      |\n" +
+                        "      |"
+        );
+
+    }
+}
