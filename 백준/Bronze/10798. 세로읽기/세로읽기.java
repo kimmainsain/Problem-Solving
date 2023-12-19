3d9ce3cb7aced5b4f69d6b62dd2fe7fec5f3c392
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2023-12-19 23:20:52 +0900

    [Bronze I] Title: 세로읽기, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        char arr[][] = new char[5][15];
+        for (int i = 0; i < 5; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                char c = arr[j][i];
+                if (c != 0) sb.append(c);
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
