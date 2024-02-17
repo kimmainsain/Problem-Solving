cfc2c7017a028016a1dcceda08a0f268d8a2a259
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-02-17 19:49:24 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 684 ms, Memory: 49040 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int INF = 1000001;
+        boolean visited[] = new boolean[INF];
+        visited[0] = true;
+        visited[1] = true;
+        for (int i = 2; i < INF; i++) {
+            for (int j = i * 2; j < INF; j = j + i) {
+                visited[j] = true;
+            }
+        }
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+            int left = 0;
+            int right = n;
+            boolean flag = false;
+            while (left <= right) {
+                if (!visited[left] && !visited[right]) {
+                    flag = true;
+                    break;
+                } else {
+                    left++;
+                    right--;
+                }
+            }
+            if (flag) sb.append(n).append(" = ").append(left).append(" + ").append(right).append("\n");
+            else sb.append("Goldbach's conjecture is wrong.").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
