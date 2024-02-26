34227f4b35ca3471b00796e56f58e4a454ef17a1
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-02-26 13:35:25 +0900

    [Silver IV] Title: 좋은 단어, Time: 172 ms, Memory: 18584 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -9,22 +9,21 @@ public class Main {
     public static void main(String[] args) throws Exception {
         int n = Integer.parseInt(br.readLine());
         int answer = 0;
+        ArrayDeque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-            Stack<Character> stack = new Stack<>();
             String str = br.readLine();
             for (int j = 0; j < str.length(); j++) {
                 char c = str.charAt(j);
                 if (stack.isEmpty()) {
                     stack.add(c);
+                } else if (stack.getLast() == c) {
+                    stack.pollLast();
                 } else {
-                    if (stack.peek() == c) {
-                        stack.pop();
-                    } else {
-                        stack.add(c);
-                    }
+                    stack.addLast(c);
                 }
             }
             if (stack.isEmpty()) answer++;
+            stack.clear();
         }
         System.out.println(answer);
     }
