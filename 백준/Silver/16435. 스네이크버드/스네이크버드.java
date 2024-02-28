d53f40e29eb0b5b7fa6743ff5aaa262d5ba19c94
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-02-28 15:21:22 +0900

    [Silver V] Title: 스네이크버드, Time: 92 ms, Memory: 11952 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+        while (!pq.isEmpty()) {
+            int now = pq.poll();
+            if (m < now) {
+                break;
+            } else {
+                m++;
+            }
+        }
+        System.out.println(m);
+    }
+}
