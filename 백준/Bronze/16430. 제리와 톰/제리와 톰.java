2c0e7ce05cf6225ab60ff9a07727f4e18c0c7f85
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-03-11 21:46:27 +0900

    [Bronze V] Title: 제리와 톰, Time: 80 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(b - a + " " + b);
+    }
+}
