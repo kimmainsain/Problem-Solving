fb363fb1509e9a49add22f74a205efd8efaf3555
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-03-25 15:16:49 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 112 ms, Memory: 9500 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.js
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.js
@@ -3,25 +3,22 @@ const FS = require("fs").readFileSync("/dev/stdin");
 const input = FS.toString();
 
 const answer = [];
+let str = "";
 
 function solution() {
   const n = Number(input);
   for (let i = 1; i < n + 1; i++) {
-    let str = "";
     str += "*".repeat(i);
     str += " ".repeat((n - i) * 2);
-    str += "*".repeat(i);
-    answer.push(str);
+    str += "*".repeat(i) + "\n";
   }
   for (let i = n - 1; i > 0; i--) {
-    let str = "";
     str += "*".repeat(i);
     str += " ".repeat((n - i) * 2);
-    str += "*".repeat(i);
-    answer.push(str)
+    str += "*".repeat(i) + "\n";
   }
 }
 
 solution();
 
-console.log(answer.join("\n"));
+console.log(str);
