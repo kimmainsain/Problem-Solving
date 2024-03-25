d1d809cbe9f00212563b2d9e1f97e92f4bea9191
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-03-25 14:31:17 +0900

    [Bronze III] Title: A+B - 6, Time: 116 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.js
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.js
@@ -1,13 +1,15 @@
 // const FS = require("fs").readFileSync("input.txt");
 const FS = require("fs").readFileSync("/dev/stdin");
 const input = FS.toString().trim().split("\n");
+const answer = [];
 
 function solution() {
   const n = Number(input[0]);
   for (let i = 0; i < n; i++) {
     const [left, right] = input[i + 1].split(",").map(Number);
-    console.log(left + right);
+    answer.push(left + right);
   }
+  console.log(answer.join("\n"));
 }
 
 solution();
