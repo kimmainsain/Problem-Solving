047059c0f720f44b1688dca5cfdb4d68eba8b029
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-03-26 23:54:01 +0900

    [Bronze IV] Title: 방학 숙제, Time: 80 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+        System.out.println(a - (int)Math.ceil(Math.max((double)b / d, (double)c / e)));
+    }
+}
