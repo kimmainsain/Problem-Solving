95c74200f1b7bdca2a1d996c072671ac4262b3f4
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-04-11 22:12:57 +0900

    [Silver III] Title: 등수 매기기, Time: 472 ms, Memory: 50812 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        long answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer += Math.abs(i + 1 - arr[i]);
+        }
+        System.out.println(answer);
+    }
+}
