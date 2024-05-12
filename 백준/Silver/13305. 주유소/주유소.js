8f847cabd77b97f9c6273f05baa14930d725bba9
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-05-12 23:12:01 +0900

    [Silver III] Title: 주유소, Time: 280 ms, Memory: 33772 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,19 +1,19 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
-const n = Number(input.shift());
-const distance = input.shift().split(" ").map(Number);
-const Liter = input.shift().split(" ").map(Number);
+const n = Number(input[0]);
+const distance = input[1].split(" ").map(Number);
+const Liter = input[2].split(" ").map(Number);
 
 let min = Number.MAX_VALUE;
-let answer = 0;
+let answer = BigInt(0);
 
 const solve = () => {
   distance.forEach((value, index) => {
     min = Math.min(min, Liter[index]);
-    answer += min * value;
+    answer += BigInt(min * value);
   });
-  console.log(answer);
+  console.log(answer.toString());
 };
 
 solve();
