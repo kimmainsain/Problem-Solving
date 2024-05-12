26e3c49b468c7ed4dcb6b1e9a4d92394708be898
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-05-12 22:29:18 +0900

    [Silver IV] Title: 로프, Time: 252 ms, Memory: 19436 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.js
+++ 백준/Silver/2217. 로프/로프.js
@@ -2,11 +2,8 @@ const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
 const n = Number(input.shift());
-const arr = input.map(Number).sort((a, b) => b - a);
-
 let max = Number.MIN_VALUE;
-let index = 1;
-while(arr.length !== 0) {
-  max = Math.max(max, arr.shift() * index++);
-}
+input.map(Number).sort((a, b) => b - a).forEach((value, index) => {
+  max = Math.max(max, value * ++index);
+});
 console.log(max);
