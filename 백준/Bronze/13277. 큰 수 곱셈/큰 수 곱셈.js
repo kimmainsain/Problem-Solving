b984e550a86b09b10e4db501ae3b76bf0be698ea
kim.mainsain <97226314+kimmainsain@users.noreply.github.com>
2024-06-22 19:14:29 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 904 ms, Memory: 14804 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.js
@@ -0,0 +1,3 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(path).toString().trim().split(" ").map(BigInt);
+console.log((input[0] * input[1]).toString());
